cd099e281961ed8e0f5616e3c304a139f4c36af7
CHI <z526634318@163.com>
2026-05-19 16:12:01 +0800

    Optimize twoSum with hash table implementation
    Refactor twoSum method to use a hash table for improved performance.

--- array/1-two-sum.py
+++ array/1-two-sum.py
@@ -1,6 +1,9 @@
 # LeetCode 1 - Two Sum
 # Difficulty: Easy
 
+solution:用两个指针往后遍历,寻找符合条件的组合.
+
+双重循环时间复杂度n^2
 
 
 class Solution:
@@ -14,3 +17,17 @@ class Solution:
                     result.append(j)
 
         return result
+
+
+#New!!!使用哈希表
+
+
+时间复杂度n,空间复杂度变高了
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashdic={}
+        for i,num in enumerate(nums):
+            need = target-num
+            if need in hashdic:
+                return [hashdic[need],i]
+            hashdic[num]=i
